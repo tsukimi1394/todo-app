05fec73b2e7c64a68177ceb1ab16794a39d35e4b
Daria Ganchukova <umi194@gmail.com>
2018-05-23 19:53:10 +0300

    upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,31 @@
 import { TestBed, async } from '@angular/core/testing';
+
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        TodoDataService
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-app!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private todoDataService: TodoDataService) { }
+
+  onAddTodo(todo) {
+  	this.todoDataService.addTodo(todo);
+  }
+
+  onToggleTodoComplete(todo) {
+  	this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  onRemoveTodo(todo) {
+  	this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+  	return this.todoDataService.getAllTodos();
+  }
+
 }
